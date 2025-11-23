ab1991fb08442d6e04dfc528872075349136d31b
trowlinson <trowlinson@gmail.com>
2025-11-23 18:01:59 +1300

    changed the search behaviour to only trigger when clicking the search button

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,30 +1,41 @@
 // Fetch the JSON index
 fetch('/search.json')
-    .then(response => response.json())
-    .then(data => {
-        // Build Lunr index
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
         var idx = lunr(function () {
-        this.field('title');
-        this.field('content');
-        this.ref('url');
+            this.field('title');
+            this.field('content');
+            this.ref('url');
 
-        data.forEach(function (doc) {
-            this.add(doc);
-        }, this);
+            data.forEach(function (doc) {
+                this.add(doc);
+            }, this);
         });
 
-        // Hook up search input
         var input = document.getElementById('search-input');
+        var button = document.getElementById('search-button');
         var results = document.getElementById('results');
 
-        input.addEventListener('input', function () {
-        var query = input.value;
-        var matches = idx.search(query);
+        button.addEventListener('click', function () {
+            var query = input.value.trim();
 
-        results.innerHTML = '';
-        matches.forEach(function (match) {
-            var item = data.find(d => d.url === match.ref);
-            results.innerHTML += `<li><a href="${item.url}">${item.title}</a></li>`;
+            results.innerHTML = '';
+
+            if (query.length === 0) {
+                results.innerHTML = '<li>Please enter a search term.</li>';
+                return;
+            }
+
+            var matches = idx.search(query);
+
+            if (matches.length === 0) {
+                results.innerHTML = '<li>No results found.</li>';
+            } else {
+                matches.forEach(function (match) {
+                    var item = data.find(function (d) { return d.url === match.ref; });
+                    results.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+                });
+            }
         });
     });
-});
+
