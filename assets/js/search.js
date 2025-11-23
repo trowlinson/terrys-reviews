cf7a398db90ffffcb522cbf6e844e1a7124fe09b
trowlinson <trowlinson@gmail.com>
2025-11-23 18:03:17 +1300

    search when pressing the enter key

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,41 +1,50 @@
-// Fetch the JSON index
 fetch('/search.json')
-    .then(function (response) { return response.json(); })
-    .then(function (data) {
-        var idx = lunr(function () {
-            this.field('title');
-            this.field('content');
-            this.ref('url');
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+      var idx = lunr(function () {
+        this.field('title');
+        this.field('content');
+        this.ref('url');
 
-            data.forEach(function (doc) {
-                this.add(doc);
-            }, this);
-        });
+        data.forEach(function(doc) {
+          this.add(doc);
+        }, this);
+      });
 
-        var input = document.getElementById('search-input');
-        var button = document.getElementById('search-button');
-        var results = document.getElementById('results');
+      var input = document.getElementById('search-input');
+      var button = document.getElementById('search-button');
+      var results = document.getElementById('results');
 
-        button.addEventListener('click', function () {
-            var query = input.value.trim();
+      function runSearch() {
+        var query = input.value.trim();
+        results.innerHTML = '';
 
-            results.innerHTML = '';
+        if (query.length === 0) {
+          results.innerHTML = '<li>Please enter a search term.</li>';
+          return;
+        }
 
-            if (query.length === 0) {
-                results.innerHTML = '<li>Please enter a search term.</li>';
-                return;
-            }
+        var matches = idx.search(query);
 
-            var matches = idx.search(query);
+        if (matches.length === 0) {
+          results.innerHTML = '<li>No results found.</li>';
+        } else {
+          matches.forEach(function(match) {
+            var item = data.find(function(d) { return d.url === match.ref; });
+            results.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+          });
+        }
+      }
 
-            if (matches.length === 0) {
-                results.innerHTML = '<li>No results found.</li>';
-            } else {
-                matches.forEach(function (match) {
-                    var item = data.find(function (d) { return d.url === match.ref; });
-                    results.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
-                });
-            }
-        });
+      // Trigger search on button click
+      button.addEventListener('click', runSearch);
+
+      // Trigger search on Enter key
+      input.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+          runSearch();
+        }
+      });
     });
+
 
